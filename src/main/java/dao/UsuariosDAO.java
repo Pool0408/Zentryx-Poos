930377314d2f36cc94efb85f94656cc7c660e77f
Pool0408 <poolponcegarcia0408@gmail.com>
2026-05-01 13:16:43 -0500

    CREACION DE UsuariosDAO para que compare los datos que ingresare con la base de datos

---
+++ src/main/java/dao/UsuariosDAO.java
@@ -0,0 +1,32 @@
+package dao;
+import model.Usuarios;
+import util.Conexion;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UsuariosDAO {
+    public Usuarios login (String usuario,String contrasena){
+        Usuarios usuario1 = null;
+        //?Crear Conexion
+        Conexion cn = new Conexion();
+        Connection con = cn.conectar();
+        String sql = "SELECT * FROM usuarios WHERE usuario = ? AND contrasena = ?";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1,usuario);
+            ps.setString(2,contrasena);
+            ResultSet rs =ps.executeQuery();
+            if (rs.next()){
+                usuario1=new Usuarios();
+                usuario1.setNombre(rs.getString("username"));
+                usuario1.setContrasena(rs.getString("contrasena"));
+            }
+        }catch (SQLException e){
+            System.out.println("Error login: "+e.getMessage());
+        }
+        return usuario1;
+    }
+}
