730e61d959348de3aa9adaee51a487cac46fe7ac
Pool0408 <poolponcegarcia0408@gmail.com>
2026-05-02 10:48:41 -0500

    Actualizacion de ProductosDAO

--- src/main/java/dao/ProductosDAO.java
+++ src/main/java/dao/ProductosDAO.java
@@ -1,36 +1,40 @@
 package dao;
 
+import model.Productos;
 import util.Conexion;
 
+import javax.swing.*;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProductosDAO {
     //!METODO LISTAR
-    public void listar(){
+    public List<Productos> listar(){
         //?Crear la conexion
         Conexion cn = new Conexion();
         Connection con = cn.conectar();
         //?Atributos
         String sql ="SELECT *FROM productos";
-        //?Mysql
+        List<Productos> lista = new ArrayList<>();
         try {
-            Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery(sql);
-            System.out.printf("%-3s| %-11s| %-6s| %-13s| %-5s%n", "ID", "Nombre", "Precio", "Categoria", "Stock");
-            System.out.println("---+------------+-------+-------------+-----");
+            //Hace la consulta
+            PreparedStatement ps =con.prepareStatement(sql);
+            //Guarda los resultados de la consulta
+            ResultSet rs = ps.executeQuery();
             while (rs.next()){
-                int id = rs.getInt("id");
-                String nombre = rs.getString("nombre");
-                double precio = rs.getDouble("precio");
-                String categoria = rs.getString("Categoria");
-                int stock = rs.getInt("stock");
-                System.out.printf("%-3d| %-11s| %-6.2f| %-13s| %-5d%n", id, nombre, precio, categoria, stock);;
+                Productos p = new Productos();
+                p.setId(rs.getInt("id"));
+                p.setNombre(rs.getString("nombre"));
+                p.setCategoria(rs.getString("categoria"));
+                p.setPrecio(rs.getDouble("precio"));
+                p.setStock(rs.getInt("stock"));
+                lista.add(p);
             }
         }catch (SQLException e){
-            System.out.println("Error a listar Productos:  "+e.getMessage());
-        }finally {
-            cn.desconectar();
+            System.out.println("Error al listar:  "+e.getMessage());
         }
+        return lista;
     }
     //!METODO AGREGAR
     public void agregar(String nombre,String categoria,double precio,int stock){
@@ -40,41 +44,55 @@ public class ProductosDAO {
         //?Atributos
         String sql ="INSERT INTO productos (nombre,precio,categoria,stock) VALUES(?,?,?,?)";
         try {
+            //Hace la consulta
             PreparedStatement ps = con.prepareStatement(sql);
             ps.setString(1,nombre);
             ps.setDouble(2,precio);
             ps.setString(3,categoria);
             ps.setInt(4,stock);
-            int filas = ps.executeUpdate();
-            if (filas>0){
-                System.out.println("Producto agregado correctamente");
-            }
+            //Modifica Datos
+            ps.executeUpdate();
         }catch (SQLException e){
             System.out.println("Error al agregar: "+e.getMessage());
-        }finally {
-            cn.desconectar();
         }
     }
     //!METODO ELIMINAR
     public void eliminar(int id){
-        //?CREAR CONEXION
+        //?Crear Conexion
         Conexion cn = new Conexion();
         Connection con = cn.conectar();
-        //?ATRIBUTOS
+        //?Atributos
         String sql="DELETE FROM productos WHERE id = ?";
         try {
+            //Hace la consulta
             PreparedStatement ps= con.prepareStatement(sql);
             ps.setInt(1,id);
-            int filas=ps.executeUpdate();
-            if (filas>0){
-                System.out.println("Producto con id : " +id+"eliminado exitosamente");
-            }else {
-                System.out.println("No se encontro el id");
-            }
+            //Modifica Datos
+            ps.executeUpdate();
         }catch (SQLException e){
             System.out.println("Error al eliminar: "+e.getMessage() );
-        }finally {
-            cn.desconectar();
         }
     }
+    //!METODO ACTUALIZAR PRODUCTO
+    public void actualizar(Productos p){
+        //?Crear conexion
+        Conexion cn = new Conexion();
+        Connection con = cn.conectar();
+        //?Atributos
+        String sql ="UPDATE productos SET nombre=? , precio=? ,categoria=?,stock=? WHERE id=?";
+        try {
+            //Hace la consulta
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, p.getNombre());
+            ps.setDouble(2, p.getPrecio());
+            ps.setString(3, p.getCategoria());
+            ps.setInt(4, p.getStock());
+            ps.setInt(5, p.getId());
+            //Modifica Datos
+            ps.executeUpdate();
+        }catch (SQLException e){
+            System.out.println("Error a actualizar producto: "+e.getMessage());
+        }
+    }
+
 }
